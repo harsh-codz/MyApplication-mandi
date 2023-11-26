a2e4c29c4ceb16d4b81f1c5ea12d5977c559873f
un_definedman <harshkethwas789@gmail.com>
2023-11-26 15:01:51 +0530

    updated now

---
+++ app/src/main/java/com/harshkethwas/myapplication/registration2.kt
@@ -0,0 +1,11 @@
+package com.harshkethwas.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registration2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration2)
+    }
+}
