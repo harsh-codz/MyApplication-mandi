59071bc46a68be37bcfd42db47b82318eb3252ad
Gourav Satore <gauravsatore22@gmail.com>
2023-11-27 21:47:53 +0530

    data oushed

--- app/src/main/java/com/harshkethwas/myapplication/URLs.kt
+++ app/src/main/java/com/harshkethwas/myapplication/URLs.kt
@@ -1,7 +1,7 @@
 package com.harshkethwas.myapplication
 
 object URLs {
-    const val urlUserLogin:String = "http://mandi.infinityfreeapp.com/htdocs/userlogin.php"
-    const val urlUsers:String = "http://mandi.infinityfreeapp.com/htdocs/users.php"
+    const val urlUserLogin:String = "https://filemanager.ai/new/#/c/185.27.134.11/if0_35449165/eyJ0IjoiZnRwIiwiYyI6eyJwIjoiZjJSb1k3cjZ4N3AyQllLIiwiaSI6IlwvIn19/htdocs/userlogin.php"
+    const val urlUsers:String = "https://filemanager.ai/new/#/c/185.27.134.11/if0_35449165/eyJ0IjoiZnRwIiwiYyI6eyJwIjoiZjJSb1k3cjZ4N3AyQllLIiwiaSI6IlwvIn19/htdocs/users.php"
 
 }
--- app/src/main/java/com/harshkethwas/myapplication/registration2.kt
+++ app/src/main/java/com/harshkethwas/myapplication/registration2.kt
@@ -4,21 +4,73 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class registration2 : AppCompatActivity() {
 
     private lateinit var btn: Button
+    private val url = URL(URLs.urlUsers)
+    private var postData:String = "";
+
+    var fullname:String = ""
+    var email:String=""
+    var city:String=""
+
+    lateinit var txtFullName:EditText
+    lateinit var txtEmail:EditText
+    lateinit var txtCity:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration2)
 
         btn = findViewById(R.id.RBtn)
+        txtFullName = findViewById(R.id.fullName)
+        txtEmail = findViewById(R.id.emailId)
+        txtCity = findViewById(R.id.city)
 
         btn.setOnClickListener {
-            startActivity(Intent(this, MainActivity::class.java))
+            if(validateEntries(txtFullName,txtEmail,txtCity))
+            {
+                GlobalScope.launch(Dispatchers.Main){
+                    addUser()
+                }
+            }
+            else
+            {
+                Toast.makeText(this,"Invalid inputs for fullname, email or city",Toast.LENGTH_LONG).show()
+            }
         }
 
+    }
+
+    private fun validateEntries(txtFullName:EditText,txtEmail:EditText,txtCity:EditText):Boolean
+    {
+        var valid:Boolean=true
+
+        if(txtFullName.text.toString().isEmpty() || txtCity.text.toString().isEmpty() || txtEmail.text.toString().isEmpty())
+        {
+            valid = false
+        }
+        else
+        {
+            fullname=txtFullName.text.toString().uppercase()
+            email = txtEmail.text.toString()
+            city = txtCity.text.toString()
+        }
+        return valid
+    }
+    private suspend fun addUser(){
+        postData = "fullname=$fullname" + "&email=$email" + "&city=$city"
+        val httpConnection = HttpConnect.HttpConnection()
+        var res = httpConnection.connect(url,postData)
+
+        Toast.makeText(this,res,Toast.LENGTH_SHORT).show()
     }
 
 }
